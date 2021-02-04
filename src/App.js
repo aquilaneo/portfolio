2739f9a6722350cab386bb3c63772581d92483b2
aquilaneo <aquilaneo@gmail.com>
2021-02-04 15:44:17 +0900

    作品の本文まで作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {Container, Row, Col} from 'react-bootstrap';
+import {useState} from "react";
 
+import {works} from "./Works";
 import "./common.css";
 import "./bootstrap/bootstrap.min.css";
 import Icon from "./img/Icon.png";
@@ -18,161 +20,411 @@ import Libraries from "./img/Libraries.png";
 import Game from "./img/Game.png";
 import Other from "./img/Other.png";
 
-function App () {
+const App = () => {
+	const [isWorkDetailVisible, setIsWorkDetailVisible] = useState (false);
+	const [selectedWork, setSelectedWork] = useState ({
+		name: "", languages: "", libraries: "", comment: <div/>
+	});
+
 	return (
-		<Container className={"container"}>
-			<div className={"space"}>
-				<div className={"inner-div"}>
-					<h1 className={"subtitle"}>Takumi's portfolio</h1>
-					<div>
-
+		<div>
+			<Container className={"container"}>
+				<div className={"space"}>
+					<div className={"inner-div"}>
+						<h1 className={"title"}>Takumi's portfolio</h1>
+						<Row>
+							<Col sm={3} className={"center top-link-container"}>
+								<a href={"#about-me"} className={"link"}>
+									<div style={{width: "100%"}}>About me</div>
+								</a>
+							</Col>
+							<Col sm={3} className={"center top-link-container"}>
+								<a href={"#activities"} className={"link"}>
+									<div style={{width: "100%"}}>Activities</div>
+								</a>
+							</Col>
+							<Col sm={3} className={"center top-link-container"}>
+								<a href={"#skills"} className={"link"}>
+									<div style={{width: "100%"}}>Skills</div>
+								</a>
+							</Col>
+							<Col sm={3} className={"center top-link-container"}>
+								<a href={"#works"} className={"link"}>
+									<div style={{width: "100%"}}>Works</div>
+								</a>
+							</Col>
+						</Row>
 					</div>
 				</div>
-			</div>
 
-			<div className={"space"}>
-				<div className={"inner-div"}>
-					<h2 className={"subtitle"}><img src={AboutMe} alt={"About me"} className={"icon"}/>About me</h2>
-					<Row>
-						<Col sm={4} style={{textAlign: "center"}}>
-							<img src={Icon} alt={"アイコン"} style={{maxWidth: "100%", maxHeight: "100%",}}/>
-						</Col>
-						<Col sm={8}>
-							<h3>Takumi</h3>
-							<div>
-								<p> 首都圏在住の大学院生です。趣味は離島めぐりやゲームです。</p>
-								<p>
-									コロナ禍で旅行に行けなくなり寂しくなりましたが、コロナ明け後は日本一の秘境とも言われる伊豆諸島の青ヶ島に行きたいと思っています。
-									好きなゲームはFinal
-									Fantasyシリーズで、「こんなすごいゲームを創ってみたい！」という想いでプログラミングを勉強し始めるきっかけになったゲームでもあります。
-								</p>
-								<p>
-									そんなことからIT/プログラミングとの出会いはゲーム開発から始まり、デスクトップアプリ開発なども体験しつつ現在はWebフロントエンド系技術に触れることが多いです。
-									大学で所属している団体やアルバイトでは組み込み開発やプログラミング教育に関わっていたりもします。
-								</p>
-							</div>
-							<div style={{textAlign: "center"}}>
-								<a href={"https://github.com/aquilaneo"}>
-									<img src={GitHubLogo} alt={"GitHub"} className={"logo"}/>
-								</a>
-								<a href={"https://qiita.com/aquilaneo"}>
-									<img src={QiitaLogo} alt={"Qiita"} className={"logo"}/>
-								</a>
-							</div>
-						</Col>
-					</Row>
-				</div>
-			</div>
-
-			<div className={"space"}>
-				<div className={"inner-div"}>
-					<h2 className={"subtitle"}><img src={Activities} alt={"Activities"} className={"icon"}/>Activities
-					</h2>
-					<Row>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Researches} alt={"Researches"} className={"icon-small"}/>Researches</h4>
-								<ul>
-									<li>学部3, 4年次は宇宙航空研究開発機構(JAXA)相模原キャンパスでレーダーと画像を用いた火星地下空洞の調査に関する研究を行う。</li>
-									<li>大学院進学後は理工系学部生向けの並列プログラミング学習ツールの開発を研究テーマとしている。</li>
-								</ul>
-							</div>
-						</Col>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Extra} alt={"Extra Activity"} className={"icon-small"}/>Extra Activity
-								</h4>
-								<ul>
-									<li>主専攻の研究とは別に、学部2年次から課外活動として東海大学情報教育センターの「ICT特定プログラム」に所属し、情報教育センターの研究室でロボット対戦ゲームをテーマとしたプログラミング学習環境を開発。</li>
-								</ul>
-							</div>
-						</Col>
-					</Row>
-					<Row>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Satellite} alt={"Satellite Project"} className={"icon-small"}/>Satellite
-									Project</h4>
-								<ul>
-									<li>東海大学衛星プロジェクト(TSP)に所属。学部2年次には3人のチームで模擬人工衛星を開発し能代宇宙イベントに出場。学部3年次には団体内の教育カリキュラムを新規に整備し、1,
-										2年生の教育を担当する。学部4年次には学生代表を務める。以降は団体で採用するマイコンの変更やGitを使用した団体内の技術ドキュメント管理体制の構築を行う。
-									</li>
-								</ul>
-							</div>
-						</Col>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Education} alt={"Education"} className={"icon-small"}/>Education</h4>
-								<ul>
-									<li>LEGO社製のロボット教材を使用した子供向け教室でアルバイト。小学1年生から高校生まで幅広い年齢層の生徒を指導(ビジュアルプログラミング環境/C言語/Pythonを使用)。所属する教室の生徒で構成されるチームがFirst
-										LEGO Leagueの世界大会に出場決定。
-									</li>
-								</ul>
-							</div>
-						</Col>
-					</Row>
-				</div>
-			</div>
-
-			<div className={"space"}>
-				<div className={"inner-div"}>
-					<h2 className={"subtitle"}><img src={Skills} alt={"Skills"} className={"icon"}/>Skills</h2>
-					<Row>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Web} alt={"Web"} className={"icon-small"}/>Web</h4>
-								<ul>
-									<li>HTML5/CSS3</li>
-									<li>JavaScript ES6</li>
-									<li>TypeScript</li>
-								</ul>
-							</div>
-						</Col>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Libraries} alt={"Libraries"} className={"icon-small"}/>Libraries</h4>
-								<ul>
-									<li>React</li>
-									<li>Electron</li>
-									<li>three.js</li>
-									<li>Blockly</li>
-								</ul>
-							</div>
-						</Col>
-					</Row>
-					<Row>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Game} alt={"Game/Desktop"} className={"icon-small"}/>Game/Desktop</h4>
-								<ul>
-									<li>Unity</li>
-									<li>C#</li>
-								</ul>
-							</div>
-						</Col>
-						<Col sm={6} style={{marginBottom: "2rem"}}>
-							<div className={"panel"}>
-								<h4><img src={Other} alt={"Other"} className={"icon-small"}/>Other</h4>
-								<ul>
-									<li>Python</li>
-									<li>C/C++</li>
-								</ul>
-							</div>
-						</Col>
-					</Row>
-				</div>
-			</div>
-
-			<div className={"space"}>
-				<div className={"inner-div"}>
-					<h2 className={"subtitle"}><img src={Works} alt={"Works"} className={"icon"}/>Works</h2>
-					<div>
-
+				<div className={"space"} id={"about-me"}>
+					<div className={"inner-div"}>
+						<h2 className={"subtitle"}><img src={AboutMe} alt={"About me"} className={"icon"}/>About me</h2>
+						<Row>
+							<Col sm={4} className={"center"}>
+								<img src={Icon} alt={"アイコン"} style={{maxWidth: "100%", maxHeight: "100%",}}/>
+							</Col>
+							<Col sm={8}>
+								<h3>Takumi</h3>
+								<div>
+									<p> 首都圏在住の大学院生です。趣味は離島めぐりやゲームです。</p>
+									<p>
+										コロナ禍で旅行に行けなくなり寂しくなりましたが、コロナ明け後は日本一の秘境とも言われる伊豆諸島の青ヶ島に行きたいと思っています。
+										好きなゲームはFinal
+										Fantasyシリーズで、「こんなすごいゲームを創ってみたい！」という想いでプログラミングを勉強し始めるきっかけになったゲームでもあります。
+									</p>
+									<p>
+										そんなことからIT/プログラミングとの出会いはゲーム開発から始まり、デスクトップアプリ開発なども体験しつつ現在はWebフロントエンド系技術に触れることが多いです。
+										大学で所属している団体やアルバイトでは組み込み開発やプログラミング教育に関わっていたりもします。
+									</p>
+								</div>
+								<div className={"center"}>
+									<a href={"https://github.com/aquilaneo"}>
+										<img src={GitHubLogo} alt={"GitHub"} className={"logo"}/>
+									</a>
+									<a href={"https://qiita.com/aquilaneo"}>
+										<img src={QiitaLogo} alt={"Qiita"} className={"logo"}/>
+									</a>
+								</div>
+							</Col>
+						</Row>
 					</div>
 				</div>
-			</div>
-		</Container>
+
+				<div className={"space"} id={"activities"}>
+					<div className={"inner-div"}>
+						<h2 className={"subtitle"}><img src={Activities} alt={"Activities"} className={"icon"}/>Activities
+						</h2>
+						<Row>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Researches} alt={"Researches"} className={"icon-small"}/>Researches
+									</h4>
+									<ul>
+										<li>学部3, 4年次は宇宙航空研究開発機構(JAXA)相模原キャンパスでレーダーと画像を用いた火星地下空洞の調査に関する研究を行う。</li>
+										<li>大学院進学後は理工系学部生向けの並列プログラミング学習ツールの開発を研究テーマとしている。</li>
+									</ul>
+								</div>
+							</Col>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Extra} alt={"Extra Activity"} className={"icon-small"}/>Extra Activity
+									</h4>
+									<ul>
+										<li>主専攻の研究とは別に、学部2年次から課外活動として東海大学情報教育センターの「ICT特定プログラム」に所属し、情報教育センターの研究室でロボット対戦ゲームをテーマとしたプログラミング学習環境を開発。</li>
+									</ul>
+								</div>
+							</Col>
+						</Row>
+						<Row>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Satellite} alt={"Satellite Project"} className={"icon-small"}/>Satellite
+										Project</h4>
+									<ul>
+										<li>東海大学衛星プロジェクト(TSP)に所属。学部2年次には3人のチームで模擬人工衛星を開発し能代宇宙イベントに出場。</li>
+										<li>学部3年次には団体内の教育カリキュラムを新規に整備し、1,2年生の教育を担当する。</li>
+										<li>学部4年次には学生代表を務め、以降は団体で採用するマイコンの変更やGitを使用した団体内の技術ドキュメント管理体制の構築を行う。</li>
+									</ul>
+								</div>
+							</Col>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Education} alt={"Education"} className={"icon-small"}/>Education</h4>
+									<ul>
+										<li>LEGO社製のロボット教材を使用した子供向け教室でアルバイト。小学1年生から高校生まで幅広い年齢層の生徒を指導(ビジュアルプログラミング環境/C言語/Pythonを使用)。</li>
+										<li>所属する教室のチームがFirst LEGO Leagueの世界大会に出場決定。</li>
+									</ul>
+								</div>
+							</Col>
+						</Row>
+					</div>
+				</div>
+
+				<div className={"space"} id={"skills"}>
+					<div className={"inner-div"}>
+						<h2 className={"subtitle"}><img src={Skills} alt={"Skills"} className={"icon"}/>Skills</h2>
+						<Row>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Web} alt={"Web"} className={"icon-small"}/>Web</h4>
+									<ul>
+										<li>HTML5/CSS3</li>
+										<li>JavaScript ES6</li>
+										<li>TypeScript</li>
+									</ul>
+								</div>
+							</Col>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Libraries} alt={"Libraries"} className={"icon-small"}/>Libraries</h4>
+									<ul>
+										<li>React</li>
+										<li>Electron</li>
+										<li>three.js</li>
+										<li>Blockly</li>
+									</ul>
+								</div>
+							</Col>
+						</Row>
+						<Row>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Game} alt={"Game/Desktop"} className={"icon-small"}/>Game/Desktop</h4>
+									<ul>
+										<li>Unity</li>
+										<li>C#</li>
+									</ul>
+								</div>
+							</Col>
+							<Col sm={6} style={{marginBottom: "2rem"}}>
+								<div className={"panel"}>
+									<h4><img src={Other} alt={"Other"} className={"icon-small"}/>Other</h4>
+									<ul>
+										<li>Python</li>
+										<li>C/C++</li>
+									</ul>
+								</div>
+							</Col>
+						</Row>
+					</div>
+				</div>
+
+				<div className={"space"} id={"works"}>
+					<div className={"inner-div"}>
+						<h2 className={"subtitle"}><img src={Works} alt={"Works"} className={"icon"}/>Works</h2>
+						<h4>研究・ICT特定プログラム</h4>
+						<Row>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["researches"][0]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["researches"][0].name}</h5>
+										<div>{works["researches"][0].languages}</div>
+										<div>{works["researches"][0].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["researches"][1]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["researches"][1].name}</h5>
+										<div>{works["researches"][1].languages}</div>
+										<div>{works["researches"][1].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["researches"][2]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["researches"][2].name}</h5>
+										<div>{works["researches"][2].languages}</div>
+										<div>{works["researches"][2].libraries}</div>
+									</div>
+								</div>
+							</Col>
+						</Row>
+
+						<h4>衛星プロジェクト</h4>
+						<Row>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["project"][0]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["project"][0].name}</h5>
+										<div>{works["project"][0].languages}</div>
+										<div>{works["project"][0].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["project"][1]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["project"][1].name}</h5>
+										<div>{works["project"][1].languages}</div>
+										<div>{works["project"][1].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["project"][2]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["project"][2].name}</h5>
+										<div>{works["project"][2].languages}</div>
+										<div>{works["project"][2].libraries}</div>
+									</div>
+								</div>
+							</Col>
+						</Row>
+
+						<h4>個人開発</h4>
+						<Row>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["private"][0]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["private"][0].name}</h5>
+										<div>{works["private"][0].languages}</div>
+										<div>{works["private"][0].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["private"][1]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["private"][1].name}</h5>
+										<div>{works["private"][1].languages}</div>
+										<div>{works["private"][1].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["private"][2]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["private"][2].name}</h5>
+										<div>{works["private"][2].languages}</div>
+										<div>{works["private"][2].libraries}</div>
+									</div>
+								</div>
+							</Col>
+						</Row>
+						<Row>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["private"][3]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["private"][3].name}</h5>
+										<div>{works["private"][3].languages}</div>
+										<div>{works["private"][3].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["private"][4]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["private"][4].name}</h5>
+										<div>{works["private"][4].languages}</div>
+										<div>{works["private"][4].libraries}</div>
+									</div>
+								</div>
+							</Col>
+							<Col sm={4} style={{marginBottom: "2rem"}}>
+								<div className={"work-panel"} onClick={() => {
+									setSelectedWork (works["private"][5]);
+									setIsWorkDetailVisible (true);
+								}}>
+									<div className={"work-image"}></div>
+									<div className={"work-title"}>
+										<h5>{works["private"][5].name}</h5>
+										<div>{works["private"][5].languages}</div>
+										<div>{works["private"][5].libraries}</div>
+									</div>
+								</div>
+							</Col>
+						</Row>
+					</div>
+				</div>
+			</Container>
+			<Container fluid className={"footer"}>
+				<Row className={"sitemap"}>
+					<Col sm={"auto"} className={"center footer-link-container"}>
+						<a href={"#about-me"} className={"link"}>
+							<div style={{width: "100%"}}>About me</div>
+						</a>
+					</Col>
+					<Col sm={"auto"} className={"center footer-link-container"}>
+						<a href={"#activities"} className={"link"}>
+							<div style={{width: "100%"}}>Activities</div>
+						</a>
+					</Col>
+					<Col sm={"auto"} className={"center footer-link-container"}>
+						<a href={"#skills"} className={"link"}>
+							<div style={{width: "100%"}}>Skills</div>
+						</a>
+					</Col>
+					<Col sm={"auto"} className={"center footer-link-container"}>
+						<a href={"#works"} className={"link"}>
+							<div style={{width: "100%"}}>Works</div>
+						</a>
+					</Col>
+				</Row>
+				<div>
+					©︎2021 Takumi
+				</div>
+			</Container>
+
+			<WorkDetail visible={isWorkDetailVisible} work={selectedWork} closeFunc={() => {
+				setIsWorkDetailVisible (false);
+			}}/>
+		</div>
 	);
-}
+};
+
+const WorkDetail = (props) => {
+	return (
+		<div className={"work-detail-panel"} style={{display: props.visible ? "block" : "none"}}>
+			<div className={"close-button"} onClick={() => {
+				props.closeFunc ();
+			}}>
+				×
+			</div>
+			<div className={"work-detail-contents"}>
+				<div className={"work-detail-image"}></div>
+				<div className={"work-detail-text"}>
+					<h4>{props.work.name}</h4>
+					<div className={"work-detail-text-paragraph"}>
+						<h5>使用技術</h5>
+						<div>{props.work.languages}</div>
+						<div>{props.work.libraries}</div>
+					</div>
+					<div className={"work-detail-text-paragraph"}>
+						<h5>解説</h5>
+						{props.work.comment}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+};
 
 export default App;
---
+++ src/Works.js
@@ -0,0 +1,162 @@
+export const works = {
+	"researches": [
+		{
+			name: "火星溶岩チューブの開発",
+			languages: "Python",
+			libraries: "",
+			comment: <div>あああ</div>,
+		},
+		{
+			name: "並列プログラミング学習ツール",
+			languages: "HTML5/CSS3/TypeScript",
+			libraries: "React/Blockly",
+			comment: <div>あああ</div>,
+		},
+		{
+			name: "Code Battler",
+			languages: "HTML5/CSS3/JavaScript ES6",
+			libraries: "three.js/Blockly",
+			comment:
+				<div>
+					<div>
+						ブロックを組み合わせて命令を作成するビジュアルプログラミング環境でロボットのプログラムを作成し、
+						ロボットをゴールへ導く、敵を倒すといった目標を達成していくプログラミング学習ツールです。
+						Webアプリとしてブラウザ上で動作し、ロボットの動作は3Dグラフィックスで描画されます。
+						また、2人のプレイヤーがそれぞれ同じ画面上でロボットのプログラムを作成し、対戦させるモードも搭載しています。
+					</div>
+					<div>
+						ICT特定プログラムの卒業制作として開発した作品で、2019年度ICT特定プログラム最優秀賞を獲得しました。
+					</div>
+				</div>,
+		}
+	],
+	"project": [
+		{
+			name: "Virtual Sat",
+			languages: "PICマイコン/C言語",
+			libraries: "Unity/C#",
+			comment:
+				<div>
+					<div>
+						PICマイコン・加速度センサ等を使用した専用コントローラーを傾けて画面上の小惑星着陸機の姿勢を制御し、
+						目標の着陸地点に安全に軟着陸させる3Dゲームです。
+						着陸時に着陸目標地点からの距離が表示されるので、他のプレイヤーと距離を競い合う遊び方も可能です。
+					</div>
+					<div>
+						2016年度東海大学建学祭と2017年度能代宇宙イベントで一般向けに展示を行い、大好評をいただきました。
+					</div>
+					<div>
+						なお、現在公開しているものは専用コントローラーではなくマウス操作で着陸機を操作できるようにしたバージョンです。
+					</div>
+				</div>,
+		},
+		{
+			name: "U-SAT/U-Connect",
+			languages: "Raspberry Pi/Python",
+			libraries: "Unity/C#",
+			comment:
+				<div>
+					<div>
+						東海大学学生ロケットプロジェクト(TSRP)が開発したハイブリッドロケットに搭載し、
+						高度数百メートルで放出される模擬人工衛星「U-SAT」とその管制アプリケーション「U-Connect」です。
+					</div>
+					<div>
+						U-SATはミッションとして「ロケットから放出後の落下中の姿勢を3DCGでリアルタイム可視化すること」を掲げており、
+						Unityで開発された管制アプリケーション「U-Connect」は現地のPC上での姿勢の可視化のほか、
+						インターネットを介した遠隔地のPCやスマートフォン上での姿勢可視化も可能であり、コマンド送信機能も備えるなど
+						模擬人工衛星の管制アプリケーションとしては非常に高い機能を持っています。
+					</div>
+					<div>
+						学生が開発したロケットの発射実験や模擬人工衛星の投下実験を合同で行う2017年度能代宇宙イベントにて打ち上げられ、
+						姿勢のリアルタイム可視化と遠隔地からの姿勢観測にも成功しました。
+					</div>
+					<div>
+						このプロジェクトは当時の学部2年生3名で開発を行い、私が模擬衛星の制御ソフトウェア及び管制ソフトウェアの開発、
+						他2名がそれぞれ模擬衛星の構体/無線通信コンポーネントの開発、模擬衛星の電装系開発を担当しました。
+					</div>
+				</div>,
+		},
+		{
+			name: "公式Webサイト",
+			languages: "HTML5/CSS3",
+			libraries: "bootstrap",
+			comment:
+				<div>
+					衛星プロジェクトの公式Webサイトです。
+					普段Web技術を扱うことがない団体であるため、引き継ぎ時を考え極力HTMLとCSSのみで作成するようにしました。
+					レスポンシブデザインに対応するためbootstrapを使用しています。
+				</div>,
+		},
+	],
+	"private": [
+		{
+			name: "TRINE Editor",
+			languages: "C#/Windows Forms",
+			libraries: "DXライブラリ",
+			comment:
+				<div>
+					プログラミング未経験でも簡単な3Dゲームを製作することが可能なWindowsデスクトップアプリです。
+					ゲームのロジックはGUIで作成でき、未経験者にはハードルが高いテキスト言語を一切記述することなくゲームの開発が可能です。
+				</div>,
+		},
+		{
+			name: "TRINE 3D",
+			languages: "HTML5/CSS3/TypeScript",
+			libraries: "Vue.js/three.js/Blockly/Electron",
+			comment:
+				<div>
+					「TRINE Editor」の発展版です。テキスト言語を一切記述せずに3Dゲームが開発できるというコンセプトはそのままに、
+					より直感的にロジックを組み立てることができるブロック型のビジュアルプログラミング環境を取り入れた3Dゲーム製作ツールとして開発中です。
+					またWeb技術でデスクトップアプリを開発できるElectronを使用しているため、前作と違いマルチプラットフォーム展開が可能であり、
+					将来的には学校などソフトウェアのダウンロード・インストールが制限された環境でも使用できるWebアプリとして公開することも検討しています。
+				</div>,
+		},
+		{
+			name: "テトリス with C++/CLI",
+			languages: "C++/CLI/Windows Forms",
+			libraries: "",
+			comment: <div>C++の勉強用に作成したテトリスです。C++/CLIとWindows Formsで開発しました。</div>,
+		},
+		{
+			name: "ポートフォリオ",
+			languages: "HTML5/CSS3/JavaScript ES6",
+			libraries: "React/bootstrap",
+			comment: <div>このポートフォリオサイトです。自己紹介やこれまでの活動、よく使用する技術、製作した作品を掲載しています。</div>,
+		},
+		{
+			name: "Poll",
+			languages: "nim",
+			libraries: "",
+			comment:
+				<div>
+					<div>
+						「I」「l」「1」の3文字からなる6命令を使い、それ以外の文字はコメントとして扱われるBrainf*ck系のネタプログラミング言語です。
+						もともとは衛星プロジェクト内のLightning Talk(LT)のネタになるかと思い、当時興味のあったnim言語で実装してみたものです。
+					</div>
+					<div>
+						「Hello World」と表示するソースコード例↓
+					</div>
+					<div style={{border: "solid thin black"}}>
+						I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1I1I1llI1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1llI1I1I1I1I1I1I1llllI1I1I1llIII1I1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1llI1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1I1I1I1I1I1I1I1llI1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1I1<br/>
+						I1I1I1I1I1I1llI1I1I1lllIlIlIlIlIlIlllIlIlIlIlIlIlIlIll
+					</div>
+				</div>,
+		},
+		{
+			name: "Space Ball",
+			languages: "Unity/C#",
+			libraries: "",
+			comment:
+				<div>
+					高校生の頃製作したUnity製ゲームです。「宇宙空間での球技」をテーマとして作成しました。
+					Unityインターハイ2015に出場し、特別出典として本選会場で展示を行いました。
+				</div>,
+		},
+	]
+}
